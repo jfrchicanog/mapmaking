c89bd10fdf217cca2ad6f3cfe28b13b8b604d8f6
francis <chicano@lcc.uma.es>
2021-03-31 12:30:58 +0200

    Adding Evostar rooms

---
+++ maps/Evostar Conference/Lounge-background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Lounge-background" tilewidth="32" tileheight="32" tilecount="1200" columns="30">
+ <image source="2b72ce03-a200-475a-972c-f519cc076c47.png" width="960" height="1280"/>
+</tileset>
