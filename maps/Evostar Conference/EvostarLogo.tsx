3fde0210ab64b028a75558713cc1ddf67b52777e
Francisco Chicano <chicano@lcc.uma.es>
2024-04-01 17:36:23 +0200

    Logo 2024

--- maps/Evostar Conference/EvostarLogo.tsx
+++ maps/Evostar Conference/EvostarLogo.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="EvostarLogo" tilewidth="32" tileheight="32" tilecount="60" columns="12">
- <image source="logo_evostar-2023.png" width="384" height="160"/>
+ <image source="logo_evostar-2024.png" width="384" height="160"/>
 </tileset>
